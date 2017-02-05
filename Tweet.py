2b17730fd4612627dae8c98f2bba704e9aacf3c0
Beichen <lbc0430@gmail.com>
2017-02-05 16:14:39 -0500

    add tweet.py

---
+++ Tweet.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3.6
+
+###############################################################################
+## File name:   Tweet.py
+## Author:      BL(02-05-2017)
+## Modified by: 
+## Description: This python script is used to push the question to server side 
+## and request for answers and post the answers as a tweet 
+##
+## TODO: install tweet
+##       communicate from between client and server.
+
+import tweepy
+
+CONSUMER_KEY = "bPqfYmH4yYQw5ZkUhuSYZIIpd";
+CONSUMER_SECRET = "IBsm0SrLSR3uKynCYppo0YDa5tThawD5h4lQ2BNFvlNYS6UhZp";
+
+ACCESS_KEY = "1456947205-4hpgqmfBh92YtMuTL60oIcFVmMVRaWp4NJYQcyA";
+ACCESS_SECRET = "IgvVo2Jg6bSN5LjholkZUHkrBnEcfHSKF4TOHNc9X3vxw";
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+api.update_status('tweepy + oauth!')
+
